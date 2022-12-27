cd8ece3089fe1dfc20362006978b26935297afde
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-27 17:22:40 +0100

    Llamada a red completada

--- app/src/main/java/com/aiglesiaspubill/internetexamplekotlin/MainActivityViewModel.kt
+++ app/src/main/java/com/aiglesiaspubill/internetexamplekotlin/MainActivityViewModel.kt
@@ -1,6 +1,31 @@
 package com.aiglesiaspubill.internetexamplekotlin
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import okhttp3.*
+import java.io.IOException
 
 class MainActivityViewModel : ViewModel() {
+
+    //LLAMADA AL BOOTCAMP
+    fun getBootcamps() {
+        val client = OkHttpClient()
+        val url = "https://dragonball.keepcoding.education/api/data/bootcamps"
+        val request = Request.Builder().url(url).build()
+        val call = client.newCall(request)
+
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(MainActivityViewModel::javaClass.name, "Error")
+            }
+            override fun onResponse(call: Call, response: Response) {
+                Log.d(MainActivityViewModel::javaClass.name, "Success")
+                val responseBody = response.body?.string()
+                println(responseBody)
+            }
+        })
+
+    }
+
+
 }
